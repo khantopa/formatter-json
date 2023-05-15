e1343a4ec263e5ede22dabce01991d66f448ca33
Mohamed Khan <mkhan377@gmail.com>
2023-05-15 10:41:18 +0800

    Disallow cgi/bin in sitemap and robot.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   siteUrl: "https://formatterjson.com",
   generateRobotsTxt: true,
-  exclude: ["/404"],
+  exclude: ["/404", "/cgi-bin/robots.txt"],
   outDir: "./out",
   generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/cgi-bin/"],
+      },
+    ],
+  },
 };

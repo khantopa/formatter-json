ccb015e5447077eb59a7d971bddc1d6184a3d17a
Mohamed Khan <mkhan377@gmail.com>
2023-05-22 17:57:47 +0800

    Adding GA scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src='https://www.googletagmanager.com/gtag/js?id=G-XCMZ0BC59E'
+      ></Script>
+      <Script strategy='lazyOnload'>
+        {`
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+ 
+   gtag('config', 'G-XCMZ0BC59E');
+  `}
+      </Script>
+      <Component {...pageProps} />{' '}
+    </>
+  );
 }

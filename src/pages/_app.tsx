d2655b08520fccbe173d91c52712a9e9aa01fbeb
Mohamed Khan <mkhan377@gmail.com>
2023-05-23 08:35:19 +0800

    Revert "Adding GA scripts"
    This reverts commit ccb015e5447077eb59a7d971bddc1d6184a3d17a.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,6 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Script
-        strategy='lazyOnload'
-        src='https://www.googletagmanager.com/gtag/js?id=G-XCMZ0BC59E'
-      ></Script>
-      <Script strategy='lazyOnload'>
-        {`
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
- 
-   gtag('config', 'G-XCMZ0BC59E');
-  `}
-      </Script>
-      <Component {...pageProps} />{' '}
-    </>
-  );
+  return <Component {...pageProps} />
 }
